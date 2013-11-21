1ad19cd04a82f1b79984514153c8b803fdea4232
lytasky <373662565@qq.com>
2013-11-21 15:04:46 +0800

    change the mySql password

--- src/dbmgr/DBConMgr.java
+++ src/dbmgr/DBConMgr.java
@@ -7,7 +7,7 @@ public class DBConMgr {
 	
 	private  String driverName="com.mysql.jdbc.Driver";      //驱动程序名
 	private  String userName="root";                         //数据库用户名
-	private  String userPwd="123";                          //密码
+	private  String userPwd="root";                          //密码
     private  String url= "jdbc:mysql://localhost:3306/bbs";      //连接字符串
     private  Connection conn=null;                                  //数据库连接对象
     public Statement sm=null;                                       //数据库语句对象   
